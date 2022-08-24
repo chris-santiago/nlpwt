08042e155814b24e07a9cf176be3de4c727b00fb
chris-santiago <cjsantiago@gatech.edu>
2022-08-24 00:14:10 +0000

    chap 8

--- env_setup.sh
+++ env_setup.sh
@@ -1,2 +1,6 @@
 pip install torch==1.12.0+cu113 torchvision==0.13.0+cu113 torchaudio==0.12.0+cu113 --extra-index-url https://download.pytorch.org/whl/cu113
-pip install transformers==4.18
+pip install transformers==4.18 optuna
+
+sudo curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs
+git lfs install
